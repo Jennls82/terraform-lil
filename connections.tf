daf885084d82979aadb5100c561b5c9ed3e426e4
Jennifer Sutherland <jennifersutherland@gmail.com>
2019-05-30 16:55:49 -0600

    creating vmand file cleanup

---
+++ connections.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  credentials = "${file("../account.json")}"
+  project = "literraformcourse"
+  region = "us-west1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+#provider "azurerm" {
+#  subscription_id = "${var.subscription_id}"
+#  client_id = "${var.client_id}"
+#  client_secret = "${var.client_secret}"
+#  tenant_id = "${var.tenant_id}"
+#}
+#these would normally go in a vars file
+#variable subscription_id {}
+#variable client_id {}
+#variable client_secret {}
+#variable tenant_id {}
